6efccc364d6a11890488938d50b20abdbafd2d2d
Micah Young <micah.young@gmail.com>
2018-02-06 16:16:02 -0500

    Install docker listening on localhost:2375

--- up.sh
+++ up.sh
@@ -62,6 +62,59 @@ chpasswd: { expire: False }
 ssh_pwauth: True
 ssh_authorized_keys:
   - $VM_AUTHORIZED_KEY
+package_upgrade: true
+packages: ['docker-ce']
+apt:
+  preserve_sources_list: true
+  sources:
+    docker-ce.list:
+      source: "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$RELEASE stable"
+      keyid: 0EBFCD88
+write_files:
+- path: /etc/dpkg/dpkg.cfg.d/excludes
+  content: |
+    path-exclude=/lib/systemd/system/docker.service
+- path: /etc/docker/daemon.json
+  content: |
+    {
+      "hosts": ["tcp://127.0.0.1:2375","unix:///var/run/docker.sock"]
+    }
+- path: /lib/systemd/system/docker.service
+  content: |
+    [Unit]
+    Description=Docker Application Container Engine
+    Documentation=https://docs.docker.com
+    After=network-online.target docker.socket firewalld.service
+    Wants=network-online.target
+    Requires=docker.socket
+    
+    [Service]
+    Type=notify
+    # the default is not to use systemd for cgroups because the delegate issues still
+    # exists and systemd currently does not support the cgroup feature set required
+    # for containers run by docker
+    ExecStart=/usr/bin/dockerd --config-file=/etc/docker/daemon.json
+    ExecReload=/bin/kill -s HUP \$MAINPID
+    LimitNOFILE=1048576
+    # Having non-zero Limit*s causes performance problems due to accounting overhead
+    # in the kernel. We recommend using cgroups to do container-local accounting.
+    LimitNPROC=infinity
+    LimitCORE=infinity
+    # Uncomment TasksMax if your systemd version supports it.
+    # Only systemd 226 and above support this version.
+    TasksMax=infinity
+    TimeoutStartSec=0
+    # set delegate yes so that systemd does not reset the cgroups of docker containers
+    Delegate=yes
+    # kill only the docker process, not all processes in the cgroup
+    KillMode=process
+    # restart the docker process if it exits prematurely
+    Restart=on-failure
+    StartLimitBurst=3
+    StartLimitInterval=60s
+    
+    [Install]
+    WantedBy=multi-user.target
 EOF
 
 xorrisofs -volid cidata -joliet -rock bin/user-data bin/meta-data > bin/cloud-init.iso
